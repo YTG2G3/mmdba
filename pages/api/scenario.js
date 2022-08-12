f6208e56b18e9e286a5aa0dbed1ca1838ebda0a0
YTG2G3 <scratchlover05@gmail.com>
2022-08-11 22:04:16 -0700

    todo- download scenario

---
+++ pages/api/scenario.js
@@ -0,0 +1,34 @@
+import { scenario } from "../../lib/firebase";
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        let scenarioName = req.query.name;
+
+        if (scenarioName) {
+            let doc = await scenario.doc(scenarioName).get();
+
+            if (doc.exists) res.status(200).json({ ...doc.data(), sid: scenarioName });
+            else res.status(400).end();
+        }
+        else {
+            let docs = await scenario.listDocuments();
+            res.status(200).json(docs.map(v => v.id));
+        }
+    }
+    else if (req.method === 'POST') {
+        let scenarioName = req.query.name;
+
+        if (scenarioName) await scenario.doc(scenarioName).set(req.body);
+        else await scenario.add(req.body);
+
+        res.status(200).end();
+    }
+    else if (req.method === 'DELETE') {
+        let scenarioName = req.query.name;
+        if (!scenarioName) return res.status(400).end();
+
+        await scenario.doc(scenarioName).delete();
+
+        res.status(200).end();
+    }
+}
